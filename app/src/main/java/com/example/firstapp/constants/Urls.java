478a869cbe7c549d3a3ac70a714fbb065dd3a5c7
kutukov2000 <kutukov16.2005@gmail.com>
2024-04-14 14:03:38 +0300

    fix: local api url 🐛

--- app/src/main/java/com/example/firstapp/constants/Urls.java
+++ app/src/main/java/com/example/firstapp/constants/Urls.java
@@ -1,6 +1,6 @@
 package com.example.firstapp.constants;
 
 public class Urls {
-        public static final String BASE = "https://vpd111.itstep.click";
-//    public static final String BASE = "http://10.0.2.2:7100";
+//        public static final String BASE = "https://vpd111.itstep.click";
+    public static final String BASE = "http://10.0.2.2:5232";
 }
